d7ba088c367f64d6645df90d6b3fcc802162f7d0
Dion507 <geraderex01@gmail.com>
2026-06-02 19:49:20 -0500

    chambea

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T5P3</title>
+    <link rel="stylesheet" href="./CSS/styles.css">
+</head>
+<nav>
+    <?php include "./HTML/nav.html" ?>
+</nav>
+<main>
+    <?php include "./HTML/main.html" ?>
+</main>
+<footer>
+    <?php include "./HTML/footer.html" ?>
+</footer>
+
+</html>
